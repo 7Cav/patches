5776064d2487bd2b648064e77bafa361e90198b1
mazinskihenry <33608576+mazinskihenry@users.noreply.github.com>
2022-06-07 15:50:48 -0700

    First commit

---
+++ Config.cpp
@@ -0,0 +1,162 @@
+class CfgPatches {
+	class WorldFlagPatches
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "1.0.0";
+		author[]= {"Hannibal"};
+	};
+
+};
+
+class CfgUnitInsignia
+{
+	class CA_c
+	{
+		displayName = "CA - Canada";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\CA_c_co.paa";
+		textureVehicle = "";
+	};
+	class CA_o
+	{
+		displayName = "CA - Canada - olive";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\CA_o_co.paa";
+		textureVehicle = "";
+	};
+	class CA_t
+	{
+		displayName = "CA - Canada - tan";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\CA_t_co.paa";
+		textureVehicle = "";
+	};
+	class GB_c
+	{
+		displayName = "GB - Great Britain";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\GB_c_co.paa";
+		textureVehicle = "";
+	};
+	class GB_o
+	{
+		displayName = "GB - Great Britain - olive";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\GB_o_co.paa";
+		textureVehicle = "";
+	};
+	class GB_t
+	{
+		displayName = "GB - Great Britain - tan";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\GB_t_co.paa";
+		textureVehicle = "";
+	};
+	class PL_c
+	{
+		displayName = "PL - Poland";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\PL_c_co.paa";
+		textureVehicle = "";
+	};
+	class PL_ct
+	{
+		displayName = "PL - Poland - text";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\PL_ct_co.paa";
+		textureVehicle = "";
+	};
+	class PL_o
+	{
+		displayName = "PL - Poland - olive";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\PL_o_co.paa";
+		textureVehicle = "";
+	};
+	class PL_ot
+	{
+		displayName = "PL - Poland - olive text";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\PL_ot_co.paa";
+		textureVehicle = "";
+	};
+	class PL_t
+	{
+		displayName = "PL - Poland - tan";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\PL_t_co.paa";
+		textureVehicle = "";
+	};
+	class PL_tt
+	{
+		displayName = "PL - Poland - tan text";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\PL_tt_co.paa";
+		textureVehicle = "";
+	};
+	class UA_c
+	{
+		displayName = "UA - Ukraine";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\UA_c_co.paa";
+		textureVehicle = "";
+	};
+	class UA_ct
+	{
+		displayName = "UA - Ukraine - text";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\UA_ct_co.paa";
+		textureVehicle = "";
+	};
+	class UA_o
+	{
+		displayName = "UA - Ukraine - olive";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\UA_o_co.paa";
+		textureVehicle = "";
+	};
+	class UA_ot
+	{
+		displayName = "UA - Ukraine - olive text";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\UA_ot_co.paa";
+		textureVehicle = "";
+	};
+	class UA_t
+	{
+		displayName = "UA - Ukraine - tan";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\UA_t_co.paa";
+		textureVehicle = "";
+	};
+	class UA_tt
+	{
+		displayName = "UA - Ukraine - tan text";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\UA_tt_co.paa";
+		textureVehicle = "";
+	};
+	class US_c
+	{
+		displayName = "US - United States of America";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\US_c_co.paa";
+		textureVehicle = "";
+	};
+	class US_o
+	{
+		displayName = "US - United States of America - olive";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\US_o_co.paa";
+		textureVehicle = "";
+	};
+	class US_t
+	{
+		displayName = "US - United States of America - tan";
+		author = "Hannibal";
+		texture = "\7Cav_Auxillary\@Cav_Aux\addons\patches\patches\icon\US_t_co.paa";
+		textureVehicle = "";
+	};
+};
